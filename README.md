e9f4d4aa9a86722628f5fcb5b9de56064fcfbe93
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-05-21 13:55:26 +0300

    Added readFromFile and getConsoleInput methods

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IOStream
 * Write and append to file
+* Read file by lines
+* Get console input
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,24 +4,51 @@ import java.io.*;
 
 public class App {
 
-    public static void main( String[] args ) throws IOException {
+    private static final String FILE_NAME = "out.txt";
 
-        String fileName = "out.txt";
+    public static void main(String[] args) throws IOException {
 
-//        writeToFile(fileName, "Hello"); // Hello
-//        appendToFile(fileName, "Woo"); // HelloWoo
-//        appendToFile(fileName, "Woo"); // HelloWooWoo
-//        appendToFile(fileName, "Woo"); // HelloWooWooWoo
-
-        appendToFile(fileName, "What"); // HelloWooWooWooWhat
+        writeToFile(FILE_NAME, getConsoleInput()); // Gets one line input from console and writes it to the file
+        System.out.println(readFromFile(FILE_NAME)); // Reads a file by lines and prints it in a separate lines
     }
 
+    /**
+     * Reads file by lines and writes these lines to a StringBuilder object. Returns this object converted to String
+     * */
+    private static String readFromFile(String fileName) throws IOException {
+
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
+    }
+
+    /**
+     * Asks user for one-line console input. Returns this input
+     * */
+    private static String getConsoleInput() throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print("Enter some text: ");
+            return bufferedReader.readLine();
+        }
+    }
+
+    /**
+     * Writes text to a file
+     * */
     private static void writeToFile(String fileName, String text) throws IOException {
         FileWriter fileWriter = new FileWriter(fileName);
         fileWriter.write(text);
         fileWriter.close();
     }
 
+    /**
+     * Appends text to a file
+     * */
     private static void appendToFile(String fileName, String text) throws IOException {
         FileWriter fileWriter = new FileWriter(fileName, true); // true for append
         fileWriter.write(text);
